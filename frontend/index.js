2a945b34791a84076a51d533d5e196dcb103edb0
zane <labs.zane@gmail.com>
2026-02-19 23:51:29 +0100

    Remaking architecture to focus on discord

--- frontend/index.js
+++ frontend/index.js
@@ -2,20 +2,12 @@
 
 
 let username;
-let discord_link;
-let tiktok_link;
-let github_link;
-let api_key;
 
 let discord_token1;
-let discord_token2;
-let discord_token3;
 
-const btc_price = "https://api.coingecko.com/api/v3/simple/price?ids=btc&vs_currencies=usd";
-const eth_price = "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd";
-const solana_price = "https://api.coingecko.com/api/v3/simple/price?ids=solana&vs_currencies=usd";
+const page = document.body.getAttribute("data-page");
 
-
+//INDEX PAGE
 if (document.getElementById("submit_username")){
 
 document.getElementById("submit_username").onclick = function(){
@@ -28,35 +20,38 @@ document.getElementById("submit_username").onclick = function(){
 }
 }
 
-
+//CONFIG PAGE
 
 else if (document.getElementById("save_config")){
 
 document.getElementById("save_config").onclick = function() {
 
 
-    tiktok_link = document.getElementById("tiktok_link").value;
-    localStorage.setItem("tiktok_link",tiktok_link);
-
-
-    github_link = document.getElementById("github_link").value;
-    localStorage.setItem("github_link",github_link);
-
-    api_key = document.getElementById("api_key").value;
-    localStorage.setItem("api_key",api_key);
-
-    discord_link = document.getElementById("discord_link").value;
-    localStorage.setItem("discord_link",discord_link);
 
 
     
     discord_token1 = document.getElementById("discord_token1").value;
     localStorage.setItem("discord_token1",discord_token1);
 
-    discord_token2 = document.getElementById("discord_token2").value;
-    localStorage.setItem("discord_token2",discord_token2);
-    
 }
 
+
+
+
+document.getElementById("access_dashboard").onclick = function() {
+
+    document.location.href = "dashboard.html"
+}
+
+
+
+}
+
+
+//DASHBOARD
+
+else if (document.getElementById("save_config")){
+
+    
 }
 
